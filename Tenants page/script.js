3242e50b43ac39a49eb3d91243603df9580e24ce
brianngwa <briano99ngwa@gmail.com>
2022-05-30 13:01:47 +0300

    Uploaded files

---
+++ Tenants page/script.js
@@ -0,0 +1,31 @@
+const hamburger = document.querySelector(
+  ".header .nav-bar .nav-list .hamburger"
+);
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const menu_item = document.querySelectorAll(
+  ".header .nav-bar .nav-list ul li a"
+);
+const header = document.querySelector(".header.wrapper");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
+});
+
+document.addEventListener("scroll", () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 150) {
+    header.style.backgroundImage =
+      "linear-gradient(60deg, #29323c 0%, #485563 100%)";
+  } else {
+    header.style.backgroundImage = "none";
+    header.style.backgroundColor = "#3b363675";
+  }
+});
+
+menu_item.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
+});
